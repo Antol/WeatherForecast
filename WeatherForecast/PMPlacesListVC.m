b700574881d65538edb9e04714d2788ecb2c6305
Antol <antol.peshkov@gmail.com>
2015-10-13 17:33:03 +0300

    Show real weather forecast

--- WeatherForecast/PMPlacesListVC.m
+++ WeatherForecast/PMPlacesListVC.m
@@ -12,11 +12,14 @@
 #import "PMPlace.h"
 #import "PMNibManagement.h"
 #import "PMPlaceTableViewCell.h"
+#import "PMWeatherForecastManager.h"
+#import "PMWeatherForecast.h"
+#import "PMCondition.h"
 
 @interface PMPlacesListVC () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
-@property (nonatomic, strong) NSArray *places;
+@property (nonatomic, strong) NSArray *forecastsForPlaces;
 @end
 
 @implementation PMPlacesListVC
@@ -24,20 +27,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    @weakify(self);
+    [RACObserve(self.forecastManager, forecasts) subscribeNext:^(id x) {
+        @strongify(self);
+        self.forecastsForPlaces = x;
+        [self.tableView reloadData];
+    }];
 }
 
 #pragma mark - UITableView
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.places.count;
+    return self.forecastsForPlaces.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     PMPlaceTableViewCell *cell = [tableView dequeueReusableCellForClass:[PMPlaceTableViewCell class] indexPath:indexPath];
-    PMPlace *place = [self.places objectAtIndex:indexPath.row];
-    cell.textLabel.text = place.name;
+    PMWeatherForecast *forecast = [self.forecastsForPlaces objectAtIndex:indexPath.row];
+    cell.textLabel.text = forecast.place.name;
+    cell.detailTextLabel.text = [forecast.currentCondition.temperatureC description];
     return cell;
 }
 
